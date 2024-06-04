3de0e4b7056700795b7f26eb5a39fbd65d4c1e79
Miko Lehtola <miko.lehtla@gmail.com>
2024-06-04 11:08:36 +0300

    dwa

--- db_config.php
+++ db_config.php
@@ -9,5 +9,5 @@ $dsn = "mysql:host=$db_host;dbname=$db_name";
 try {
     $db_connection = new PDO($dsn, $db_username, $db_password);
 } catch(Exception $e) {
-    echo "There was a failure - " . $e->getMessage();
+    echo "There was failure - " . $e->getMessage();
 }
